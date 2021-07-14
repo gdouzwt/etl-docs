8943447d175bda5bc94bee1ab07c3e686c33d9f9
招文桃 <gdouzwt@gmail.com>
2021-07-14 19:38:54 +0800

    done with 案场转化率分析

---
+++ run.ps1
@@ -0,0 +1,7 @@
+﻿bundle exec jekyll clean
+bundle exec jekyll build
+cd ./_site
+7z a good.zip .
+scp good.zip seedland_test:/data/www-html/
+curl -H "Content-Type: application/json" https://test-hr-srpt.seedland.cc/hooks/etl-update
+cd ..
